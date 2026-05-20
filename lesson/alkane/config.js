760ff9e77a09fe725719c7d664224acff5cbc170
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-05-20 23:18:24 +0700

    Update config.js

--- lesson/alkane/config.js
+++ lesson/alkane/config.js
@@ -1,1 +1,36 @@
+window.lessonConfig = {
 
+  id:"organic_2",
+
+  title:"Alkane Lesson",
+
+  pdf:
+    "./alkane/lesson.pdf",
+
+  totalQuestions:15,
+
+  score:{
+
+    start:150,
+
+    correct:20,
+
+    wrong:-5,
+
+    hint:-15
+
+  },
+
+  nft:{
+
+    enabled:true,
+
+    gold:420,
+
+    silver:360,
+
+    bronze:0
+
+  }
+
+};
