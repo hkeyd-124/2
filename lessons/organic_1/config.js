3ea37df68a82cbb291e766edbc007015bbb638ff
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-05-20 22:40:55 +0700

    Create config.js

---
+++ lessons/organic_1/config.js
@@ -0,0 +1,34 @@
+window.lessonConfig = {
+
+  id:"organic_1",
+
+  title:"Organic Lesson 1",
+
+  pdf:
+    "lessons/organic_1/lesson.pdf",
+
+  scoreConfig:{
+
+    start:150,
+
+    correct:20,
+
+    wrong:-5,
+
+    hint:-15
+
+  },
+
+  nft:{
+
+    enabled:true,
+
+    gold:420,
+
+    silver:360,
+
+    bronze:0
+
+  }
+
+};
