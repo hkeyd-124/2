3c378a2a2ea7ab637d1356f0cac9c7b4a2be6bed
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-05-26 23:37:55 +0700

    Update questions.js

--- lessons/organic_1/questions.js
+++ lessons/organic_1/questions.js
@@ -3,35 +3,153 @@ window.lessonQuestions = [
   {
 
     type:"single",
+q:"Alkane là các hydrocarbon",
+opt:["mạch vòng","mạch hở","mạch hở, chỉ có liên kết đơn C-C và C-H","mạch vòng, chứa liên kết C-C, C=C và C-H"],
+a:2
+},
 
-    q:"Hợp chất hữu cơ là gì?",
+{
+type:"single",
+q:"Công thức phân tử nào sau đây không phải là công thức của một alkane?",
+opt:["C<sub>2</sub>H<sub>6</sub>","C<sub>3</sub>H<sub>6</sub>","C<sub>4</sub>H<sub>10</sub>","C<sub>5</sub>H<sub>12</sub>"],
+a:1
+},
 
-    opt:[
-      "A",
-      "B",
-      "C",
-      "D"
-    ],
+{
+type:"single",
+q:"Trong công nghiệp alkane có ứng dụng là?",
+opt:["Làm nhiên liệu, nguyên liệu","Làm thực phẩm","Làm hương liệu","Làm mỹ phẩm"],
+a:0
+},
 
-    a:0
+{
+type:"single",
+q:"Biogas là một loại khí sinh học, được sản xuất bằng cách ủ kín các chất thải hữu cơ trong chăn nuôi, sinh hoạt. Biogas được dùng để đun nấu, chạy máy phát điện sinh hoạt gia đình. Thành phần chính của biogas là?",
+opt:["N<sub>2</sub>","CO<sub>2</sub>","CH<sub>4</sub>","NH<sub>3</sub>"],
+a:2
+},
 
-  },
+{
+type:"single",
+q:"Đốt cháy hoàn toàn 12,395 lít khí methane (đkc). Thể tích khí oxygen (đkc) cần dùng là?",
+opt:["123,95","317,85","540","743,7"],
+a:1
+},
 
-  {
+/* TRUE FALSE */
+{
+type:"truefalse",
+q:"Nhận định nào đúng/sai trong các nhận định sau:",
+opt:[
+"Các hydrocarbon chỉ có liên kết đơn trong phân tử là alkane",
+"Chỉ các alkane là chất khí ở điều kiện thường được dùng làm nhiên liệu",
+"Các alkane lỏng được dùng sản xuất xăng, dầu và làm dung môi",
+"Khi đốt cháy alkane sinh ra sản phẩm gồm CO<sub>2</sub> và H<sub>2</sub>O"
+],
+a:[false,false,true,true]
+},
 
-    type:"single",
+/* SHORT */
+{
+type:"short",
+q:"Đốt cháy hoàn toàn m gam butane (C<sub>4</sub>H<sub>10</sub>), thu được tổng thể tích CO2 và hơi nước là 22,311 lít (đkc). Tìm giá trị của m (làm tròn đến hàng phần mười)",
+a:"5.8"
+},
 
-    q:"Alkane thuộc loại nào?",
+/* SINGLE tiếp */
+{
+type:"single",
+q:"Trong phân tử methane có bao nhiêu liên kết?",
+opt:["4 liên kết đơn C – H","1 liên kết đôi C = H và 3 liên kết đơn C – H","2 liên kết đơn C – H và 2 liên kết đôi C = H","1 liên kết đơn C – H và 3 liên kết đôi C = H"],
+a:0
+},
 
-    opt:[
-      "Hydrocarbon no",
-      "Hydrocarbon thơm",
-      "Alcohol",
-      "Acid"
-    ],
+{
+type:"single",
+q:"Khẳng định nào sau đây là không đúng",
+opt:[
+"Methane là chất khí nhẹ hơn không khí",
+"Methane là nguồn cung cấp hydrogen cho công nghiệp sản xuất phân bón hoá học",
+"Methane là chất khí cháy được trong không khí và toả nhiều nhiệt",
+"Methane là chất khí nhẹ hơn khí hydrogen"
+],
+a:3
+},
 
-    a:0
+{
+type:"single",
+q:"Vì sao một số alkane được sử dụng làm nhiên liệu?",
+opt:[
+"Vì giá thành rẻ",
+"Vì khi cháy tỏa nhiệt mạnh",
+"Vì khi cháy tạo CO<sub>2</sub> và H<sub>2</sub>O",
+"Vì sử dụng nhiên liệu này sẽ không gây ô nhiễm môi trường"
+],
+a:1
+},
 
-  }
+{
+type:"single",
+q:"Công thức cấu tạo thu gọn của propane là?",
+opt:["CH<sub>4</sub>","CH<sub>3</sub>-CH<sub>3</sub>","CH<sub>3</sub>-CH<sub>2</sub>-CH<sub>3</sub>","CH<sub>3</sub>-(CH<sub>2</sub>)<sub>2</sub>-CH<sub>3</sub>"],
+a:2
+},
+
+/* SHORT */
+{
+type:"short",
+q:`
+Cho phương trình phản ứng đốt cháy butane:<br><br>
+
+<div style="text-align:center; display:flex; justify-content:center; align-items:center; gap:10px;">
+
+  <span>aC<sub>4</sub>H<sub>10</sub> + bO<sub>2</sub></span>
+
+  <span style="display:flex; flex-direction:column; align-items:center;">
+    <span style="font-size:12px;">t<sup>°</sup></span>
+    <span>&rarr;</span>
+  </span>
+
+  <span>cCO<sub>2</sub> + dH<sub>2</sub>O</span>
+
+</div><br>
+
+Nếu a = 2 thì b + c bằng bao nhiêu?
+`,
+a:"13"
+},
+
+{
+type:"single",
+q:"Phát biểu nào sau đây về ứng dụng của alkane không đúng?",
+opt:[
+"Propane và butane được sử dụng làm nhiên liệu",
+"Các alkane ở trạng thái lỏng là nguyên liệu để sản xuất một số hydrocarbon thơm",
+"Các alkane lỏng được sử dụng làm nhiên liệu như xăng hay dầu diesel",
+"Các alkane ở trạng thái lỏng và rắn được dùng làm nến và sáp"
+],
+a:1
+},
+
+{
+type:"single",
+q:"Trong phân tử propane có số liên kết đơn là",
+opt:["10","9","8","11"],
+a:0
+},
+
+/* TRUE FALSE */
+{
+type:"truefalse",
+q:"Nhận định nào đúng/sai trong các nhận định sau:",
+opt:[
+"Methane là alkane đơn giản nhất",
+"Propane có công thức C₃H₆",
+"Alkane có thể cháy sinh nhiệt.",
+"Ethane là chất khí"
+],
+a:[true,false,true,true]
+}
+
 
 ];
