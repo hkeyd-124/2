11cb864b66a331f3392375d4e4e9ac27e5c11e51
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-05-26 14:08:41 +0700

    Update questions.js

--- lessons/organic_1/questions.js
+++ lessons/organic_1/questions.js
@@ -1,1 +1,37 @@
+window.lessonQuestions = [
 
+  {
+
+    type:"single",
+
+    q:"Hợp chất hữu cơ là gì?",
+
+    opt:[
+      "A",
+      "B",
+      "C",
+      "D"
+    ],
+
+    a:0
+
+  },
+
+  {
+
+    type:"single",
+
+    q:"Alkane thuộc loại nào?",
+
+    opt:[
+      "Hydrocarbon no",
+      "Hydrocarbon thơm",
+      "Alcohol",
+      "Acid"
+    ],
+
+    a:0
+
+  }
+
+];
