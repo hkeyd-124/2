c74ec801c05019b0d1d96d4882ccb949b4b4c59f
hkeyd-124 <nhatkycuavuong2@gmail.com>
2026-05-29 22:04:38 +0700

    Create certificateLayout.js

---
+++ js/certificateLayout.js
@@ -0,0 +1,77 @@
+window.certificateLayout = {
+
+  width: 1536,
+
+  height: 1024,
+
+  avatar:{
+
+    x: 240,
+
+    y: 560,
+
+    size: 260
+
+  },
+
+  studentName:{
+
+    x: 760,
+
+    y: 470,
+
+    align:"center",
+
+    fontSize:72
+
+  },
+
+  lessonName:{
+
+    x: 760,
+
+    y: 665,
+
+    align:"center",
+
+    fontSize:44
+
+  },
+
+  score:{
+
+    x: 1230,
+
+    y: 575,
+
+    align:"center",
+
+    fontSize:80
+
+  },
+
+  date:{
+
+    x: 330,
+
+    y: 900,
+
+    align:"left",
+
+    fontSize:34
+
+  },
+
+  tier:{
+
+    x: 1360,
+
+    y: 120,
+
+    align:"center",
+
+    fontSize:48
+
+  }
+
+};
